8fb3317076e6c30862086f1b851bfdf1fef53ecd
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-15 03:26:47 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// Define your Book class here:
+class book = {
+  constructor(title, author, copyright, isbn, pageCount, timesCheckedOut, discarded)
+  this.title = title;
+  this.author = author;
+  this.copyright = copyright;
+  this.isbn = isbn;
+  this.pageCount = pageCount:,
+  this.timesCheckedOut = timesCheckedOut;
+  this.discarded = discarded:
+}
+
+// Define your Manual and Novel classes here:
+let maual = {
+  title:'Top Secret Shuttle Building Manual',
+  author:'Redacted',
+  copyright-date:2013,
+  isbn:0000000000000,
+  pageCount:1147,
+  timesCheckedOut:1,
+  discarded:'No'
+}
+
+let novel = {
+  title:'Pride and Prejudice',
+  author:'Jane Austen',
+  copyright-date:1813,
+  isbn:1111111111111,
+  pageCount:432,
+  timesCheckedOut:32,
+  discarded:'No'
+}
+// Declare the objects for exercises 2 and 3 here:
+
+
+// Code exercises 4 & 5 here:
