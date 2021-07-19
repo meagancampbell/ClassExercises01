2bce99314e7e5937f2b8bf5351865a9b6ac3ce6f
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-07-19 16:38:26 +0000

    mc

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
 // Define your Book class here:
-class book = {
-  constructor(title, author, copyright, isbn, pageCount, timesCheckedOut, discarded)
-  this.title = title;
-  this.author = author;
-  this.copyright = copyright;
-  this.isbn = isbn;
-  this.pageCount = pageCount:,
-  this.timesCheckedOut = timesCheckedOut;
-  this.discarded = discarded:
-}
-
-// Define your Manual and Novel classes here:
-class Manual extends Book{
-  constructor (title, author, copyright, isbn, pageCount, timesCheckedOut, discarded)
+class Book {
+  constructor(title, author, copyright, isbn, pageCount, timesCheckedOut, discarded){
+    this.title = title;
+    this.author = author;
+    this.copyright = copyright;
+    this.isbn = isbn;
+    this.pageCount = pageCount;
+    this.timesCheckedOut = timesCheckedOut;
+    this.discarded = discarded;
+  }
+  checkedOut(){
+      if (this.timesCheckedOut > 100  ){
+        this.discarded = 'Yes';
+      }
+  }
 };
 
-class Novel extends Book{
-  constructor (title, author, copyright,isbn, pageCount, timesCheckedOut, discarded)
+  // Define your Manual and Novel classes here:
+  class Manual extends Book{
+  constructor (title, author, copyright, isbn, pageCount, timesCheckedOut, discarded) {
+    super (title, author, copyright, isbn, pageCount, timesCheckedOut, discarded)
+  }
+}
+  class Novel extends Book{
+  constructor (title, author, copyright,isbn, pageCount, timesCheckedOut, discarded){
+  super (title, author, copyright, isbn, pageCount, timesCheckedOut, discarded)
+  }
 };
 // Declare the objects for exercises 2 and 3 here:
-let topSecretShuttleBuildingManual = new Manual {
-  title:'Top Secret Shuttle Building Manual',
-  author:'Redacted',
-  copyright:2013,
-  isbn:0000000000000,
-  pageCount:1147,
-  timesCheckedOut:1,
-  discarded:'No'
-}
+let topSecretShuttle = new Manual (
+  'Top Secret Shuttle Building Manual', 'Redacted', 2013,0000000000000, 1147, 1, 'No')
 
-let prideandPrejudice = new Novel {
-  title:'Pride and Prejudice',
-  author:'Jane Austen',
-  copyright:1813,
-  isbn:1111111111111,
-  pageCount:432,
-  timesCheckedOut:32,
-  discarded:'No'
-}
+let prideandPrejudice = new Novel (
+  'Pride and Prejudice', 'Jane Austen', 1813, 1111111111111, 432, 32, 'No')
 
 // Code exercises 4 & 5 here:
+
+console.log(topSecretShuttle);
+console.log(prideandPrejudice);
+console.log(topSecretShuttle.timesCheckedOut);
+console.log(prideandPrejudice.timesCheckedOut);
